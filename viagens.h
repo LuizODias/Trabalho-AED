95d46b9dc82b59b1df3908237c4a126a7ee51474
José Augusto Bolina <joseaugusto.bolina@hotmail.com>
2015-11-14 23:04:27 -0200

    Update viagens.h

--- viagens.h
+++ viagens.h
@@ -14,12 +14,17 @@ struct deslocamento{
 };
 typedef struct deslocamento Deslocamento;
 
+struct viag{
+    char cidade[30],horas[15];
+    struct viag *ant, *prox,*migs;
+};
+typedef struct viag Viagens;
+
 struct data{
     char data[10];
-    struct data *ant, *prox;
+    struct data *ant, *prox,*viage;
 };
 typedef struct data Data;
-
 
 //CRIA AS LISTAS
 Data* criaNo();
@@ -29,9 +34,13 @@ Amigo* criaAmigo();
 //ADICIONAR DADOS
 Amigo* insereAmigo(Amigo* l, char* nome, char* telefone, char* email);
 Deslocamento* insereDeslocamento(Deslocamento* l, char* cidade, char* pais, char* estado);
-Data* insereData(Data* l, char* dia);
+Data* insereData(Data* l, char* dia,Amigo* all_friends, Deslocamento* all_places);
 
 //CHECAR ELEMENTOS REPETIDOS DA LISTA
 int checaAmigos(Amigo* l, char* nome);
 int checaViagem(Deslocamento* l, char* cidade);
 int checaData(Data *l, char* dia);
+
+//INSERIR NA VIAGEM
+Viagens* amigoNaViagem(Viagens* l,Amigo* all_friends);
+Data* insereViagem(Data* l,Amigo* all_friends, Deslocamento* all_places);
